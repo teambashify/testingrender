faf736dc29fc81597008c5461be03190975cafb2
teambashify <116701118+teambashify@users.noreply.github.com>
2022-12-18 18:35:58 +0530

    c

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const {createServer} = require('http')
 
 const app = express();
 
+
+const PORT = process.env.PORT || 4000;
+
 const httpserverv =  createServer(app);
 
 
@@ -12,6 +15,6 @@ app.get("/",(req,res)=>{
     res.json({'connected':true});
 })
 
-httpserverv.listen(4000,()=>{
+httpserverv.listen(PORT,()=>{
     console.log("Server is listening on port : 4000");
 })
